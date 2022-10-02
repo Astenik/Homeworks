b4526024968aa475f1bbd5a25dd7482d6da9c8fd
Astenik <106985461+Astenik@users.noreply.github.com>
2022-10-02 14:03:02 +0400

    Update day3.py

--- day3.py
+++ day3.py
@@ -101,24 +101,109 @@ print(f"palindrome numbers are: {res}")
 
 
 
+# 4-th task
+
+'''
+   this project produces a list of the gaps
+   between consecutive entries in L.
+   Then find the maximum gap size and
+   the percentage of gaps that have 
+   size 2.
+'''
+
+size = int(input("insert the size of your list: "));
+
+L = [None] * size;
+for num in range(size):
+     L[num] = int(input("insert L's num: "));
+
+gaps = [];
+_max = 0;
+for i in range(size - 1):
+     for j in range(i + 1, size):
+         k = L[j] - L[i];
+         gaps.append(k);
+         _max = max(_max, k);
+
+print(f"the max gap is: {_max}")
+
+count = 0;
+for num in gaps:
+     if num == 2:
+          count += 1;
+
+percentage = count * 100 / len(gaps);
+
+print(f"the percentage of 2's in gaps is: {percentage}")
 
 
 
+# 5-th task
 
 
+count = int(input("How many products you have? "))
+
+products_and_pices = {} 
+
+for i in range(count):
+     name = input("insert product name: ")
+     pice = int(input("insert product pice: "))
+     products_and_pices[name] = pice;
+
+num = input("insert the product name you want to know the pice: ")
+
+if num in products_and_pices:
+      print(f"your product pice is: {products_and_pices[num]}")
+
+else:
+      print("your product is not in list.")
 
 
+# 6-th task
 
+count = int(input("insert the count of users: "));
 
+users = {};
 
+for i in range(count):
+     name = input("insert the user name: ");
+     phone = input("insert the user phone: ");
+     email = input("insert the user email: ");
+     users[f'user{i}'] = { 'name': name,
+                  'phone': phone,
+                  'email' : email }
 
+for user in users:
+     if users[user]['email'] == '':
+          print("user that don't have email.")
+          print(users[user]['name']);
+     if users[user]['phone'][-1] == '8':
+          print("user whos phone number ends with 8.")
+          print(users[user]['name']);
 
+            
+# 7-th task
 
+matrix = [];
+L = []
+for i in range(5):
+     for j in range(5):
+          L.append(int(input("insert number: ")))
+     matrix.append(L)
 
+counts = {};
 
+for i in range(5):
+     for j in range(5):
+         num = matrix[i][j]
+         if f"num{num}" in counts:
+              counts[f"num{num}"] += 1;
+         else:
+              counts[f"num{num}"] = 1; 
 
+res = [];
+for num in counts:
+    res.append(counts[num]);
 
-
-
-
-
+sorted(res);
+print(res[:3])
