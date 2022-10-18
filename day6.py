abe449867a51491a2025b31d954dd2a9899f369a
Astenik <106985461+Astenik@users.noreply.github.com>
2022-10-18 21:39:02 +0400

    Update day6.py

--- day6.py
+++ day6.py
@@ -61,7 +61,29 @@ def  is_win(board: list):
     return False
   
   
-  
+#task 3
+
+def merge(nums1: list, nums2: list) -> list:
+    i = j = 0
+    res = []
+    while (i < len(nums1)) and (j < len(nums2)):
+        if nums1[i] < nums2[j]:
+            res.append(nums1[i]);
+            i += 1;
+        else:
+            res.append(nums2[j]);
+            j += 1;
+    
+    while i < len(nums1):
+        res.append(nums1[i])
+        i += 1 
+    
+    while j < len(nums2):
+        res.append(nums2[j])
+        j += 1
+    
+    return res 
+
   
   
   
